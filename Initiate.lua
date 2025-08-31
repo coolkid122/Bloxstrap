fd96716dabba9a7de0ec7ee5c1c89e604aa6cd9c
coolkid122 <140546530+coolkid122@users.noreply.github.com>
2025-08-31 11:01:43 -0500

    Update Initiate.lua

--- Initiate.lua
+++ Initiate.lua
@@ -1,37 +1,48 @@
 
 local hidegui = getgenv().hideui or false
-local cloneref: () -> () = cloneref or function(...): (...any) -> (...any) return (...) end
-local httpservice = cloneref(game:GetService('HttpService'))
-local getasync: () -> () = function(string: string): (string) -> (string)
-    return game:HttpGet(string, true)
-end
-makefolder('Bloxstrap');
-makefolder('Bloxstrap/Main');
-    makefolder('Bloxstrap/Main/Functions');
-    makefolder('Bloxstrap/Main/Configs');
-    makefolder('Bloxstrap/Main/Fonts')
-    makefolder('Bloxstrap/Images')
-local install: () -> () = function(config: {path: string, setup: boolean}): (table) -> ()
-    config = config or {}
-    
-    for i: number, v: table in httpservice:JSONDecode(getasync('https://api.github.com/repos/coolkid122/Bloxstrap/contents/')) do
-        if v.name:find('.lua') then
-            writefile(`Bloxstrap/{v.name}`, `return loadstring(game:HttpGet('https://raw.githubusercontent.com/coolkid122/Bloxstrap/refs/heads/main/{v.name}', true))()`);
-        elseif v.name:find('.mp3') or v.name:find('.png') then
-            writefile(`Bloxstrap/{v.name}`, game:HttpGet(`https://raw.githubusercontent.com/coolkid122/Bloxstrap/refs/heads/main/{v.name}`));
-        end;
-    end;
-    writefile(`Bloxstrap/Main/Bloxstrap.lua`, `return loadstring(game:HttpGet('https://raw.githubusercontent.com/coolkid122/Bloxstrap/refs/heads/main/Main/Bloxstrap.lua', true))()`);
-    for i: number, v: table in httpservice:JSONDecode(getasync('https://api.github.com/repos/coolkid122/Bloxstrap/contents/Main/Functions')) do
-        writefile(`Bloxstrap/Main/Functions/{v.name}`, `return loadstring(game:HttpGet('https://raw.githubusercontent.com/coolkid122/Bloxstrap/refs/heads/main/Main/Functions/{v.name}', true))()`);
-    end;
-    writefile("Bloxstrap/Main/Configs/Default.json", "{}")
-end;
 
-if (not isfolder('Bloxstrap') or #listfiles('Bloxstrap') <= 6) then
+local cloneref = cloneref or function(x) return x end
+local httpservice = cloneref(game:GetService("HttpService"))
+
+local function getasync(url)
+    return game:HttpGet(url, true)
+end
+
+if not isfolder("Bloxstrap") then
+    makefolder("Bloxstrap")
+    makefolder("Bloxstrap/Main")
+    makefolder("Bloxstrap/Main/Functions")
+    makefolder("Bloxstrap/Main/Configs")
+    makefolder("Bloxstrap/Main/Fonts")
+    makefolder("Bloxstrap/Images")
+end
+
+local function install(config)
+    config = config or {}
+
+    local mainFiles = httpservice:JSONDecode(getasync("https://api.github.com/repos/coolkid122/Bloxstrap/contents/"))
+    for i, v in pairs(mainFiles) do
+        if v.name:find(".lua") then
+            writefile("Bloxstrap/"..v.name, "return loadstring(game:HttpGet('https://raw.githubusercontent.com/coolkid122/Bloxstrap/refs/heads/main/"..v.name.."', true))()")
+        elseif v.name:find(".mp3") or v.name:find(".png") then
+            writefile("Bloxstrap/"..v.name, game:HttpGet("https://raw.githubusercontent.com/coolkid122/Bloxstrap/refs/heads/main/"..v.name))
+        end
+    end
+
+    writefile("Bloxstrap/Main/Bloxstrap.lua", "return loadstring(game:HttpGet('https://raw.githubusercontent.com/coolkid122/Bloxstrap/refs/heads/main/Main/Bloxstrap.lua', true))()")
+
+    local funcFiles = httpservice:JSONDecode(getasync("https://api.github.com/repos/coolkid122/Bloxstrap/contents/Main/Functions"))
+    for i, v in pairs(funcFiles) do
+        writefile("Bloxstrap/Main/Functions/"..v.name, "return loadstring(game:HttpGet('https://raw.githubusercontent.com/coolkid122/Bloxstrap/refs/heads/main/Main/Functions/"..v.name.."', true))()")
+    end
+
+    writefile("Bloxstrap/Main/Configs/Default.json", "{}")
+end
+
+if not isfolder("Bloxstrap") or #listfiles("Bloxstrap") <= 6 then
     install({})
 end
 
-local Bloxstrap: table = loadfile('Bloxstrap/Main/Bloxstrap.lua')()
-Bloxstrap.start() 
+local Bloxstrap = loadfile("Bloxstrap/Main/Bloxstrap.lua")()
+Bloxstrap.start()
 Bloxstrap.Visible(not hidegui)
