c745f1bb8e9b4caf5e8cca4c7274646fda958f95
coolkid122 <140546530+coolkid122@users.noreply.github.com>
2025-08-31 09:36:37 -0500

    Update ToggleFFlag.lua

--- Main/Functions/ToggleFFlag.lua
+++ Main/Functions/ToggleFFlag.lua
@@ -1,17 +1,25 @@
 local cloneref = cloneref or function(...) return ... end
 local HttpService = cloneref(game.GetService(game, "HttpService"))
-return function(flag: string, value: string): (string, string) -> ()
-	local type = type or typeof
-	if type(flag) ~= "string" then return task.spawn(error, "string expected, got "..type(flag)) end
-	local FFlag: string = Bloxstrap.TouchEnabled and flag:gsub("DFInt", ""):gsub("DFFlag", ""):gsub("FFlag", ""):gsub("FInt", ""):gsub("DFString", ""):gsub("FString", "") or flag --> Removes the keyword of the FFlag, setfflag doesn't like those so we will need to remove it.
-	
-	if getfflag(FFlag) ~= nil then
-		local fflagfile = HttpService:JSONDecode(readfile("Bloxstrap/FFlags.json"))
-		fflagfile[flag] = tostring(value)
-		writefile("Bloxstrap/FFlags.json", HttpService:JSONEncode(fflagfile))
-		return setfflag(FFlag, tostring(value))
-	else
-		local err = isfile(errorlog) and readfile(errorlog) or "Error while loading FFlags: "
-		return writefile(errorlog, err.."\nFFlag expected, got "..FFlag)
-	end
+return function(flag: string, value: any): (string, any) -> ()
+    local type = type or typeof
+    if type(flag) ~= "string" then return task.spawn(error, "string expected, got "..type(flag)) end
+    
+    local cleanFlag = flag:gsub("^[DF]*Flag", ""):gsub("^[DF]*Int", ""):gsub("^[DF]*String", "")
+    
+    local stringValue
+    if type(value) == "boolean" then
+        stringValue = value and "true" or "false"
+    else
+        stringValue = tostring(value)
+    end
+
+    if pcall(function() return getfflag(cleanFlag) end) then
+        local fflagfile = HttpService:JSONDecode(readfile("Bloxstrap/FFlags.json"))
+        fflagfile[flag] = stringValue
+        writefile("Bloxstrap/FFlags.json", HttpService:JSONEncode(fflagfile))
+        return setfflag(cleanFlag, stringValue)
+    else
+        local err = isfile(errorlog) and readfile(errorlog) or "Error while loading FFlags: "
+        return writefile(errorlog, err.."\nFFlag expected, got "..cleanFlag)
+    end
 end;
