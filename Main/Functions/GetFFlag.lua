f61a46351955b12997654191e74c5bcf7471acfe
coolkid122 <140546530+coolkid122@users.noreply.github.com>
2025-08-31 09:35:37 -0500

    Update GetFFlag.lua

--- Main/Functions/GetFFlag.lua
+++ Main/Functions/GetFFlag.lua
@@ -1,10 +1,12 @@
-return function(flag: string, value: string?): (string, string?) -> (string?)
-	if type(flag) ~= "string" then return task.spawn(error, "string expected, got "..type(flag)) end
-	local FFlag: string = Bloxstrap.TouchEnabled and flag:gsub("DFInt", ""):gsub("DFFlag", ""):gsub("FFlag", ""):gsub("FInt", ""):gsub("DFString", ""):gsub("FString", "") or flag --> Removes the keyword of the FFlag, setfflag doesn't like those so we will need to remove it.
-	
-	if getfflag(FFlag) ~= nil then
-		return getfflag(FFlag)
-	else
-		return task.spawn(error, "FFlag expected, got "..FFlag)
-	end
+return function(flag: string): (string) -> (string?)
+    if type(flag) ~= "string" then return task.spawn(error, "string expected, got "..type(flag)) end
+    
+    local cleanFlag = flag:gsub("^[DF]*Flag", ""):gsub("^[DF]*Int", ""):gsub("^[DF]*String", "")
+    
+    local value = getfflag(cleanFlag)
+    if value ~= nil then
+        return value
+    else
+        return task.spawn(error, "FFlag expected, got "..cleanFlag)
+    end
 end
